64b12ee522926963ad0134b288501744790363ce
Ryudas <sivlesegrob9@gmail.com>
2020-03-17 07:46:26 +0100

    Changed EDF

--- src/part2/Scheduler_P_EDF.c
+++ src/part2/Scheduler_P_EDF.c
@@ -4,6 +4,7 @@
 static void ExecuteTask (Taskp t)
 {
   /* insert code */
+  t->Invoked++;
   t->Taskf(t->ExecutionTime); // execute task
   /* insert code */
 }
@@ -15,11 +16,19 @@ void Scheduler_P_EDF (Task Tasks[])
   uint8_t high_prio_idx = 0;
   for (int i = 0; i < NUMTASKS; ++i)
   {
-  	if(Tasks[i].NextPendingDeadline < Tasks[high_prio_idx].NextPendingDeadline )
-  		high_prio_idx = i;
+
+	    Taskp t = &Tasks[i]; 
+	  	// check ready 
+	  	if (t->Activated != t->Invoked)
+	  	{
+	  		  	
+		  	if(t->NextPendingDeadline < Tasks[high_prio_idx].NextPendingDeadline )
+				high_prio_idx = i;
+	  	}
+
+
   }
 
-  if(Tasks[high_prio_idx]>Activated != Tasks[high_prio_idx]->Invoked)
   	ExecuteTask(&Tasks[high_prio_idx]);
 
 }
