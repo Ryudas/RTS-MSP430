beb234502cf6e396499f7ef0ce4133fc99b59704
Zack <heatedphoenix@gmail.com>
2020-03-17 16:26:15 +0100

    The shitty scheduler

--- src/part2/Scheduler_P_FP.c
+++ src/part2/Scheduler_P_FP.c
@@ -1,28 +1,45 @@
 #include "Scheduler.h"
 #include "Led.h"
+#include "TimeTracking.h"
 
-static void ExecuteTask (Taskp t)
+static void ExecuteTask(Taskp t)
 {
-  /* ----------------------- INSERT CODE HERE ----------------------- */
-
-  t->Invoked++;
-  t->Taskf(t->ExecutionTime); // execute task
-
-  /* ---------------------------------------------------------------- */
-
+	t->Invoked++;
+	t->Taskf(t->ExecutionTime); // execute task
+	t->Flags ^= BUSY_EXEC;
 }
 
-void Scheduler_P_FP (Task Tasks[])
-{ 
-  /* ----------------------- INSERT CODE HERE ----------------------- */
+void Scheduler_P_FP(Task Tasks[])
+{
+	int i;
+	for (i = 0; i < NUMTASKS; i++)
+	{
+		StartTracking(TT_SCHEDULER);
+		Taskp t = &Tasks[i];
+		if (t->Flags & BUSY_EXEC)
+		{
+			StopTracking(TT_SCHEDULER);
+			PrintResults();
+			break;
+		}
+		else
+		{
+			if (!(t->Flags & TRIGGERED))
+			{
+				t->Activated = t->Invoked;
+			}
 
-  /* Super simple, single task example */
-  Taskp t = &Tasks[0];
-  if (t->Activated != t->Invoked)
-  {
-    ExecuteTask(t);
-  }
-  /* End of example*/
-
-  /* ---------------------------------------------------------------- */
+			while (t->Activated != t->Invoked)
+			{
+				t->Flags |= BUSY_EXEC;
+				_EINT();
+				StopTracking(TT_SCHEDULER);
+				ExecuteTask(t);
+				StartTracking(TT_SCHEDULER);
+				_DINT();
+				StopTracking(TT_SCHEDULER);
+			}
+		}
+		PrintResults();
+	}
 }
